d40f16c9955fc71f59814369759049880aad261a
Mario Guerra <manachoyaso@gmail.com>
2022-04-08 20:21:53 -0400

    botton for change idiom

--- parts/sidenav.js
+++ parts/sidenav.js
@@ -5,3 +5,15 @@ console.log(sideBarButton);
 sideBarButton.addEventListener("click", () => {
     sidebar.classList.toggle("close")
 });
+
+let check = document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id = check.checked;
+    if(id !== true){
+        location.href="es/index.html"
+    }else if (id !== false){
+        location.href="../sideNav.html"
+    }
+};
