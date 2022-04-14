8f93b55cdb6d2bef6ed73c6500e1e39855e0664e
Mario Guerra <manachoyaso@gmail.com>
2022-04-14 17:16:59 -0400

    created gallery pictures

---
+++ parts/galleryApp.js
@@ -0,0 +1,28 @@
+//show and close modal
+const modalViews = document.querySelectorAll('.services__modal');
+const modalButtons = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal_close');
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active_modal');
+};
+modalButtons.forEach((modalButton, i) => {
+    modalButton.addEventListener('click',() =>{
+        modal(i)
+    })
+});
+modalCloses.forEach((modalClose,i) =>{
+    modalClose.addEventListener('click',() =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active_modal')
+        })
+    })
+});
+
+var elem = document.querySelector('.gallery--container');
+imagesLoaded( elem, () =>{
+    var msnry = new Masonry( elem, {
+        // options
+        itemSelector: '.gallery--item',
+        columnWidth: 248
+      });
+});
